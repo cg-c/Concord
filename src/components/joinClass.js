0928f0a3976533fa9d28518a1f8d5d1e06304136
webbuf <zwebb@ufl.edu>
2022-04-15 14:56:57 -0400

    join course

--- src/components/joinClass.js
+++ src/components/joinClass.js
@@ -1,31 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TextField, DialogActions } from '@mui/material';
 import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useUserAuth } from '../context/userAuthContext';
+import { updateDoc, getDoc, doc, arrayUnion } from "firebase/firestore"; 
+import {db} from "../firebase/firebaseConfig";
 
 const JoinClass = () => {
 
+    const [courseId, setCourseId] = useState('');
+    const {user} = useUserAuth();
+    const navigate = useNavigate();
 
+    async function joinClass(e){
+        e.preventDefault();
+        const docRef = doc(db, "Course", courseId);
+        await getDoc(docRef).then((documentSnapshot) => {
+            if(documentSnapshot.exists){
+                try{
+                    updateDoc(docRef, {
+                        students: arrayUnion(user.email)
+                    })
+                    const userRef = doc(db, "User", user.email);
+                    updateDoc(userRef, {
+                        joinedCourses : arrayUnion(documentSnapshot.data().courseCode)
+                    })
+                    navigate(`/course/?courseId=${courseId}`);
+                }
+                catch(error){
+                    console.log(error);
+                }
+                //this intentionally throws an error to prevent joining oops
+            }
+            else{
+                console.log("No such course")
+            }
+        })
+    }
 
 
     return (
-
-        <div>
-
+        <div className='form'>
             <p>
-                Enter your course code to join a class:
+                Enter a course ID to join:
             </p>
 
-            <TextField 
-                id='standard-required'  
-                varient='standard' 
-                label='Course Code' 
-            />
+            <div>
+                <TextField 
+                    id='standard-required' 
+                    label='Class ID' 
+                    varient='standard' 
+                    value={courseId}
+                    onChange={(e) => setCourseId(e.target.value)}
+                />
+            </div>
 
-            <DialogActions>
-                <Button>
-                    Join
-                </Button>
-            </DialogActions>
+            <Button onClick={joinClass}>
+                Join
+            </Button>
+
 
         </div>
     );
